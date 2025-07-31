4c35353a66045f6368589ab7c05b33afa0f7c96f
GustavoL1005 <krupp.gustavo@escola.pr.gov.br>
2025-07-31 12:10:34 -0300

    Add files via upload

--- katchau/perguntas.js
+++ katchau/perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Musculação',
+    'O Que é Powerlift?',
+    'O powerlifting é uma categoria de competição de força focada em três exercícios principais: agachamento, supino e levantamento terra.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'História',
+    'Quem foi Arsene Lupin?',
+    'Arsène Lupin é uma personagem fictícia francesa criada por Maurice Leblanc.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Qual o Time mais Antigo do Mundo?',
+    'O clube de futebol mais antigo do mundo é o Sheffield Football Club, fundado na Inglaterra em 1857. Ele é reconhecido pela FIFA como o clube de futebol mais antigo do Mundo. '
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Musculação',
+    'Quem foi o Maior Fisiculturista do Mundo?',
+    'O título de maior fisiculturista do mundo é frequentemente atribuído a Ronnie Coleman, devido ao seu recorde de oito títulos consecutivos no Mr. Olympia, um feito que ele compartilha com Lee Haney. Além dos títulos, a popularidade e o reconhecimento de Coleman no esporte o estabeleceram como uma figura lendária. '
 )
